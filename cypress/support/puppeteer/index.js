d77516c3cf3dd1ecf012e9a62623ba48d54638ee
Walmyr <wlsf82@gmail.com>
2023-12-14 00:03:55 +0100

    Add login puppeteer command

--- cypress/support/puppeteer/index.js
+++ cypress/support/puppeteer/index.js
@@ -36,6 +36,21 @@ module.exports = function puppeteerSetup(on) {
         // Return the heading 2 text and it will be the value yielded by the `cy.puppeteer()` invocation in the spec
         return headingTwoText
       },
+      async switchToTabAndLogin (browser) {
+        const page = await retry(async () => {
+          const pages = await browser.pages()
+          const page = pages.find(page => page.url().includes('login.html'))
+
+          if (!page) throw new Error('Could not find page')
+
+          return page
+        })
+
+        await page.bringToFront()
+
+        const loginButton = await page.waitForSelector('button')
+        return loginButton.click()
+      },
     },
   })
 }
